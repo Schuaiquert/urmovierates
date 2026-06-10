9075b03da4135b4ab9921d4a0f37560f3edd6945
OpenClaude <openclaude@gitlawb.com>
2026-06-10 17:23:30 -0300

    feat(apiKey): add barrel export

---
+++ src/apiKey/index.ts
@@ -0,0 +1,3 @@
+export { apiKeyAuth, default } from './middleware';
+export { API_KEY, API_KEY_HEADER, MIN_API_KEY_LENGTH, ApiKeyConfigError } from './config';
+export { isValidApiKey } from './secureCompare';
